75fb51be1fe4b49ced412dc8a34fd0c402b002df
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2025-04-18 10:53:51 +0300

    Update UniversityCard.jsx

--- src/components/UniversityCard.jsx
+++ src/components/UniversityCard.jsx
@@ -5,11 +5,11 @@ const UniversityCard = ({ university, onReviewClick, isProfile }) => {
     <div className="bg-cream-light rounded-xl shadow-2xl m-4 p-6 w-64 text-center">
       <h2 className="text-brown text-xl font-semibold mb-2">{university.name}</h2>
       <p className="text-light-brown mb-2">{university.location}</p>
-      <p className="text-brown font-bold mb-2"><span role="img">⭐</span> {university.overall_rating.toFixed(1)}/5</p>
-      <p className="text-brown text-sm mb-1">Academics: {university.academic_rating.toFixed(1)}/5</p>
-      <p className="text-brown text-sm mb-1">Facilities: {university.facilities_rating.toFixed(1)}/5</p>
-      <p className="text-brown text-sm mb-1">Social Life: {university.social_life_rating.toFixed(1)}/5</p>
-      <p className="text-brown text-sm mb-1">Career Prospects: {university.career_prospects_rating.toFixed(1)}/5</p>
+      <p className="text-brown font-bold mb-2"><span role="img">⭐</span> {university.overall_rating.toFixed(1)}/10</p>
+      <p className="text-brown text-sm mb-1">Academics: {university.academic_rating.toFixed(1)}/10</p>
+      <p className="text-brown text-sm mb-1">Facilities: {university.facilities_rating.toFixed(1)}/10</p>
+      <p className="text-brown text-sm mb-1">Social Life: {university.social_life_rating.toFixed(1)}/10</p>
+      <p className="text-brown text-sm mb-1">Career Prospects: {university.career_prospects_rating.toFixed(1)}/10</p>
       <p className="text-brown text-sm mb-1">Cost of Living: Ksh. {university.cost_of_living} per day</p>
       {isProfile ? (
         <button 
